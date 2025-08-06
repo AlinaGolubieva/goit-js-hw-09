bfff9bd5df240e56d4548b688a736c166afcb42e
Alina Golubieva <alya270290@gmail.com>
2025-08-06 18:22:46 +0300

    done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,35 @@
-console.log('form');
+let formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+
+if (localStorage.getItem('feedback-form-state')) {
+  formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  form.email.value = formData.email;
+  form.message.value = formData.message;
+}
+
+form.addEventListener('input', function (event) {
+  if (event.target.name === 'email') {
+    formData.email = event.target.value;
+  }
+  if (event.target.name === 'message') {
+    formData.message = event.target.value;
+  }
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+});
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+  } else {
+    console.log(formData);
+    localStorage.removeItem('feedback-form-state');
+    formData = { email: '', message: '' };
+    form.reset();
+  }
+});
